a53345632de6bfd1b0388c121501b764fbffcb37
Salim <salimbaskoy@gmail.com>
2020-09-28 20:57:26 +0300

    ad

---
+++ index.ts
@@ -0,0 +1,25 @@
+
+var app=require("express")();
+var http=require("http").createServer(app);
+var io=require("socket.io")(http); 
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html")
+})
+ 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on("sendmessage",(msg)=>{
+        console.log("message:"+msg)
+        socket.broadcast.emit('hi');
+        io.emit("sendmessage",msg);
+    })
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+
+http.listen(3000,()=>{
+    console.log("listening 3000 port")
+})

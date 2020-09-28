dd0285ee35b476acd900741d6db0de149b20a5b4
Salim <salimbaskoy@gmail.com>
2020-09-28 21:04:09 +0300

    asdad

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
     });
   });
 
-
-http.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
+http.listen(PORT,()=>{
     console.log("listening 3000 port")
 })
